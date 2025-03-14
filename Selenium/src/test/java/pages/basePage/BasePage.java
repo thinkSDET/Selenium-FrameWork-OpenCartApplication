d9ac130d00d8847a41bbcc461899f05cc7b677f5
Qa-Automation30 <qautomation32@gmail.com>
2025-03-14 20:07:14 +0530

    Major changes

---
+++ Selenium/src/test/java/pages/basePage/BasePage.java
@@ -0,0 +1,15 @@
+/*
+ * *
+ *  * Copyright (c) 2025 [thinkSDET]
+ *  * Unauthorized copying, distribution, modification, or use of this file, via any medium, is strictly prohibited.
+ *  * Proprietary and confidential.
+ *
+ *
+ */
+
+package pages.basePage;
+
+public class BasePage {
+    // isko refactor karna hai --> common package ka code yaha rakhna hai
+    // fir jitne bhi page hai wo is page ko extend karege
+}
